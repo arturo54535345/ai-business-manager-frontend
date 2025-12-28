f4579f03cec0b7ce5364bd2ce49647c0af3b7439
Jorge Arturo <titusmeaverocha@gmail.com>
2025-12-28 23:52:03 +0100

    he creado el protector osea el pertero el que protejo el AuthContext

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,23 @@
+//vamos a crear el portero el cual preguntara al AuthContext 
+import {Negative, Outlet} from 'react-router-dom';
+import {useAuth} from '../context/AuthContext';//le pedimos ayuda al cerebro
+
+const ProtectedRoute = () => {
+    const {user, loading} = useAuth();//extraemos la info del cerebro 
+
+    //si el cerebro aun piensa saldra esto 
+    if(loading){
+        return <div className="flex justify-center items-center h-screen">Cargando...</div>;
+    };
+
+    //si no hay usuario (nadie ha iniciado sesion)
+    if(!user){
+        //le mandariamos directamente a la pagina de login
+        return <Negative to="/login" replace />;
+    }
+
+    //si todo esta bien lo dejo pasat a la pagina que ese cliente queria ver 
+    return <Outlet />;
+};
+
+export default ProtectedRoute;
